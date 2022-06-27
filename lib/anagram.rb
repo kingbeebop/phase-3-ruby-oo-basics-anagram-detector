3487fa29231c29e0707007ef287b17b371885989
Brendan Ward <kingbeebop@gmail.com>
2022-06-27 16:47:58 -0400

    commit tree ab1c8091d878d634a54a8cda8f3bf546954ee465

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possible_matches)
+        result = []
+        possible_matches.each do |match|
+            if @word.chars.sort == match.chars.sort
+                result.push match
+            end
+        end
+        return result
+    end
+
+end
